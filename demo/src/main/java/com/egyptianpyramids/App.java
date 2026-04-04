93b82ce54dd2cce77144661da4ab3e92207a1a1c
Renn Le <rennlttmy@gmail.com>
2026-04-04 14:50:59 -0700

    Revert "Deleted App.java and AppTest.java"
    This reverts commit 7d6755fbfb53c53c7d44d205a909a9aefe963c03.

---
+++ demo/src/main/java/com/egyptianpyramids/App.java
@@ -0,0 +1,14 @@
+package com.egyptianpyramids;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        EgyptianPyramidsApp app = new EgyptianPyramidsApp();
+        app.start();
+    }
+}
